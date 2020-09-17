e3b27bd8adc57c5d20bc0799820b67766a3c0e92
Brigham Andersen <brighamband@gmail.com>
2020-09-17 13:20:26 -0600

    Done with string!

--- Lab1/StringAutomaton.h
+++ Lab1/StringAutomaton.h
@@ -11,24 +11,35 @@ class StringAutomaton : public Automaton {
     StringAutomaton(tokenType typeOfToken) : Automaton(typeOfToken) {}
     int read(const string &input) {
         int inputRead = 0;
+        unsigned currIndex = 1;  // start loop at 2nd value
         bool terminated = false;
 
         if (input.front() == '\'') {
             inputRead++;
 
-            // start loop at 2nd value
-            for (unsigned int i = 1; i < input.size() && !terminated; i++) {
-                if (input.at(i) == '\'') {
-                    // If it's not an apostrophe, then string is done and terminated
-                    if (input.at(i + 1) != '\'') {
-                        terminated = true;
-                        // FIXME - ANY OTHER WAY TO DO THIS?
-                        type = STRING;
-                    }
-                } else if (input.at(i) == '\n') {
+            while (currIndex < input.size()) {
+                // CASE 1 - new line
+                if (input.at(currIndex) == '\n') {
                     newLines++;
                 }
-                inputRead++;
+                // CASE 2 - apostrophe
+                else if (input.at(currIndex) == '\'' && input.at(currIndex + 1) == '\'') {
+                    inputRead += 2;
+                    currIndex += 2;
+                }
+                // CASE 3 - valid string terminated
+                else if (input.at(currIndex) == '\'') {
+                    if ((currIndex + 1) == input.size() || input.at(currIndex + 1) != '\'') {
+                        inputRead++;
+                        terminated = true;
+                        break;
+                    }
+                }
+                // CASE 4 - characters within string
+                else {
+                    inputRead++;
+                    currIndex++;
+                }
             }
         }
 
