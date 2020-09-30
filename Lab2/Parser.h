97684e4fc777d2605a5ce89bd66a33e723813583
Brigham Andersen <brighamband@gmail.com>
2020-09-29 18:34:47 -0600

    simplified

--- Lab2/Parser.h
+++ Lab2/Parser.h
@@ -1,7 +1,7 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-// #include <string>
+#include <string>
 #include <vector>
 
 #include "Token.h"
@@ -14,23 +14,6 @@ class Parser {
    public:
     Parser(vector<Token> lexerTokens) {
         tokenVctr = lexerTokens;
-        // automatonVector.push_back(new MatcherAutomaton(COMMA, ","));
-        // automatonVector.push_back(new MatcherAutomaton(PERIOD, "."));
-        // automatonVector.push_back(new MatcherAutomaton(Q_MARK, "?"));
-        // automatonVector.push_back(new MatcherAutomaton(LEFT_PAREN, "("));
-        // automatonVector.push_back(new MatcherAutomaton(RIGHT_PAREN, ")"));
-        // automatonVector.push_back(new MatcherAutomaton(COLON, ":"));
-        // automatonVector.push_back(new MatcherAutomaton(COLON_DASH, ":-"));
-        // automatonVector.push_back(new MatcherAutomaton(MULTIPLY, "*"));
-        // automatonVector.push_back(new MatcherAutomaton(ADD, "+"));
-        // automatonVector.push_back(new MatcherAutomaton(SCHEMES, "Schemes"));
-        // automatonVector.push_back(new MatcherAutomaton(FACTS, "Facts"));
-        // automatonVector.push_back(new MatcherAutomaton(RULES, "Rules"));
-        // automatonVector.push_back(new MatcherAutomaton(QUERIES, "Queries"));
-        // // custom automata
-        // automatonVector.push_back(new IDAutomaton(ID));
-        // automatonVector.push_back(new StringAutomaton(STRING));
-        // automatonVector.push_back(new CommentAutomaton(COMMENT));
     }
     void parse() {
         try {
