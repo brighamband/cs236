725b59ad547e51d99d882cd8de65023d1bde189a
Brigham Andersen <brighamband@gmail.com>
2020-10-16 07:07:04 -0600

    Added bool operator idea

--- Lab3/Tuple.h
+++ Lab3/Tuple.h
@@ -13,9 +13,12 @@ class Tuple {
     // Tuple(string paramVal) {
     //     value = paramVal;
     // }
-    bool operator<(const Tuple& other) const {
-        // compare this Tuple to other Tuple
-    }
+    // bool operator<(const Tuple& other) const {
+    //     // compare this Tuple to other Tuple
+    // }
+    // bool operator<(const Tuple b) const {
+    //     return data < b.data;
+    // }
     // void setValue(string paramVal) {
     //     value = paramVal;
     // }
